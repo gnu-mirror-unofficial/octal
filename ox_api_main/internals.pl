1d27879925061a722c4d5ed0d9cbf2dbbbd39080
webcvs <webcvs>
2001-02-25 10:58:02 +0000

    update software/octal

---
+++ ox_api_main/internals.pl
@@ -0,0 +1,42 @@
+# LaTeX2HTML 98.2 beta6 (August 14th, 1998)
+# Associate internals original text with physical files.
+
+
+$key = q/multitrack/;
+$ref_files{$key} = "$dir".q|ox_api_main.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/parameters/;
+$ref_files{$key} = "$dir".q|ox_api_main.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/flattened/;
+$ref_files{$key} = "$dir".q|ox_api_main.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/paramtable/;
+$ref_files{$key} = "$dir".q|ox_api_main.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/memory/;
+$ref_files{$key} = "$dir".q|ox_api_main.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/yourself/;
+$ref_files{$key} = "$dir".q|ox_api_main.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/ox_create/;
+$ref_files{$key} = "$dir".q|ox_api_main.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/ox_init/;
+$ref_files{$key} = "$dir".q|ox_api_main.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/paramspecs/;
+$ref_files{$key} = "$dir".q|ox_api_main.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
---
+++ ox_api_main/labels.pl
@@ -0,0 +1,85 @@
+# LaTeX2HTML 98.2 beta6 (August 14th, 1998)
+# Associate labels original text with physical files.
+
+
+$key = q/multitrack/;
+$external_labels{$key} = "$URL/" . q|ox_api_main.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/parameters/;
+$external_labels{$key} = "$URL/" . q|ox_api_main.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/flattened/;
+$external_labels{$key} = "$URL/" . q|ox_api_main.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/paramtable/;
+$external_labels{$key} = "$URL/" . q|ox_api_main.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/memory/;
+$external_labels{$key} = "$URL/" . q|ox_api_main.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/yourself/;
+$external_labels{$key} = "$URL/" . q|ox_api_main.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/ox_create/;
+$external_labels{$key} = "$URL/" . q|ox_api_main.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/ox_init/;
+$external_labels{$key} = "$URL/" . q|ox_api_main.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/paramspecs/;
+$external_labels{$key} = "$URL/" . q|ox_api_main.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
+
+# LaTeX2HTML 98.2 beta6 (August 14th, 1998)
+# labels from external_latex_labels array.
+
+
+$key = q/multitrack/;
+$external_latex_labels{$key} = q|1.6.7|; 
+$noresave{$key} = "$nosave";
+
+$key = q/parameters/;
+$external_latex_labels{$key} = q|1.3|; 
+$noresave{$key} = "$nosave";
+
+$key = q/flattened/;
+$external_latex_labels{$key} = q|1.8|; 
+$noresave{$key} = "$nosave";
+
+$key = q/paramtable/;
+$external_latex_labels{$key} = q|3.3|; 
+$noresave{$key} = "$nosave";
+
+$key = q/memory/;
+$external_latex_labels{$key} = q|1.7|; 
+$noresave{$key} = "$nosave";
+
+$key = q/yourself/;
+$external_latex_labels{$key} = q|1.4|; 
+$noresave{$key} = "$nosave";
+
+$key = q/ox_create/;
+$external_latex_labels{$key} = q|1.6.2|; 
+$noresave{$key} = "$nosave";
+
+$key = q/ox_init/;
+$external_latex_labels{$key} = q|1.6.1|; 
+$noresave{$key} = "$nosave";
+
+$key = q/paramspecs/;
+$external_latex_labels{$key} = q|1.3|; 
+$noresave{$key} = "$nosave";
+
+1;
+
