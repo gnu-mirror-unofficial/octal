2d536c077eae66aee1db1e048eb3927be94c8452
dto <dto>
2001-03-25 03:55:26 +0000

    Updated page with new news.

--- ox_api_main/labels.pl
+++ ox_api_main/labels.pl
@@ -1,16 +1,16 @@
-# LaTeX2HTML 98.2 beta6 (August 14th, 1998)
+# LaTeX2HTML 99.2beta5 (1.38)
 # Associate labels original text with physical files.
 
+
+$key = q/flattened/;
+$external_labels{$key} = "$URL/" . q|ox_api_main.html|; 
+$noresave{$key} = "$nosave";
 
 $key = q/multitrack/;
 $external_labels{$key} = "$URL/" . q|ox_api_main.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/parameters/;
-$external_labels{$key} = "$URL/" . q|ox_api_main.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/flattened/;
+$key = q/paramspecs/;
 $external_labels{$key} = "$URL/" . q|ox_api_main.html|; 
 $noresave{$key} = "$nosave";
 
@@ -30,31 +30,31 @@ $key = q/ox_create/;
 $external_labels{$key} = "$URL/" . q|ox_api_main.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/ox_init/;
+$key = q/parameters/;
 $external_labels{$key} = "$URL/" . q|ox_api_main.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/paramspecs/;
+$key = q/ox_init/;
 $external_labels{$key} = "$URL/" . q|ox_api_main.html|; 
 $noresave{$key} = "$nosave";
 
 1;
 
 
-# LaTeX2HTML 98.2 beta6 (August 14th, 1998)
+# LaTeX2HTML 99.2beta5 (1.38)
 # labels from external_latex_labels array.
 
 
-$key = q/multitrack/;
-$external_latex_labels{$key} = q|1.6.7|; 
-$noresave{$key} = "$nosave";
-
-$key = q/parameters/;
-$external_latex_labels{$key} = q|1.3|; 
-$noresave{$key} = "$nosave";
-
 $key = q/flattened/;
-$external_latex_labels{$key} = q|1.8|; 
+$external_latex_labels{$key} = q|1.9|; 
+$noresave{$key} = "$nosave";
+
+$key = q/multitrack/;
+$external_latex_labels{$key} = q|1.7.1|; 
+$noresave{$key} = "$nosave";
+
+$key = q/paramspecs/;
+$external_latex_labels{$key} = q|1.3|; 
 $noresave{$key} = "$nosave";
 
 $key = q/paramtable/;
@@ -62,7 +62,7 @@ $external_latex_labels{$key} = q|3.3|;
 $noresave{$key} = "$nosave";
 
 $key = q/memory/;
-$external_latex_labels{$key} = q|1.7|; 
+$external_latex_labels{$key} = q|1.8|; 
 $noresave{$key} = "$nosave";
 
 $key = q/yourself/;
@@ -73,12 +73,12 @@ $key = q/ox_create/;
 $external_latex_labels{$key} = q|1.6.2|; 
 $noresave{$key} = "$nosave";
 
-$key = q/ox_init/;
-$external_latex_labels{$key} = q|1.6.1|; 
+$key = q/parameters/;
+$external_latex_labels{$key} = q|1.3|; 
 $noresave{$key} = "$nosave";
 
-$key = q/paramspecs/;
-$external_latex_labels{$key} = q|1.3|; 
+$key = q/ox_init/;
+$external_latex_labels{$key} = q|1.6.1|; 
 $noresave{$key} = "$nosave";
 
 1;
