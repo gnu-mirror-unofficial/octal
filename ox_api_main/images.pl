e20ca890d67b6b8e04f6569c8d885bbe23ee0e52
dto <dto>
2001-03-25 03:56:53 +0000

    Updated docs.

---
+++ ox_api_main/images.pl
@@ -0,0 +1,30 @@
+# LaTeX2HTML 99.2beta5 (1.38)
+# Associate images original text with physical files.
+
+
+$key = q/x;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="13" HEIGHT="14" ALIGN="BOTTOM" BORDER="0"
+ SRC="img1.png"
+ ALT="$x$">|; 
+
+$key = q/+1.0;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="37" HEIGHT="28" ALIGN="MIDDLE" BORDER="0"
+ SRC="img4.png"
+ ALT="$+1.0$">|; 
+
+$key = q/(0.0,1.0);MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="64" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
+ SRC="img2.png"
+ ALT="$(0.0, 1.0)$">|; 
+
+$key = q/-1.0;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="37" HEIGHT="28" ALIGN="MIDDLE" BORDER="0"
+ SRC="img3.png"
+ ALT="$-1.0$">|; 
+
+1;
+
