f2a7e62e68e473a992082a09c285dca7bbf27903
dto <dto>
2001-04-03 18:06:02 +0000

    Minor updates, PDF docs...

--- ox_api_main/internals.pl
+++ ox_api_main/internals.pl
@@ -1,16 +1,16 @@
-# LaTeX2HTML 98.2 beta6 (August 14th, 1998)
+# LaTeX2HTML 99.2beta5 (1.38)
 # Associate internals original text with physical files.
 
+
+$key = q/flattened/;
+$ref_files{$key} = "$dir".q|ox_api_main.html|; 
+$noresave{$key} = "$nosave";
 
 $key = q/multitrack/;
 $ref_files{$key} = "$dir".q|ox_api_main.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/parameters/;
-$ref_files{$key} = "$dir".q|ox_api_main.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/flattened/;
+$key = q/paramspecs/;
 $ref_files{$key} = "$dir".q|ox_api_main.html|; 
 $noresave{$key} = "$nosave";
 
@@ -30,11 +30,11 @@ $key = q/ox_create/;
 $ref_files{$key} = "$dir".q|ox_api_main.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/ox_init/;
+$key = q/parameters/;
 $ref_files{$key} = "$dir".q|ox_api_main.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/paramspecs/;
+$key = q/ox_init/;
 $ref_files{$key} = "$dir".q|ox_api_main.html|; 
 $noresave{$key} = "$nosave";
 
